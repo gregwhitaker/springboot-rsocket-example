a5297bca081944053f196cd9d37effc45c7c0092
gregwhitaker <gwhitake@gmail.com>
2019-11-02 09:45:57 -0700

    Adding springboot application classes

---
+++ letter-service/src/main/java/example/service/letter/Application.java
@@ -0,0 +1,8 @@
+package example.service.letter;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+}
---
+++ number-service/src/main/java/example/service/number/Application.java
@@ -0,0 +1,8 @@
+package example.service.number;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    
+}
