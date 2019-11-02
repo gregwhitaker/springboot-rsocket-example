d32eeb78be042470d415874ff2f22ea6b4012da6
gregwhitaker <gwhitake@gmail.com>
2019-11-02 13:06:13 -0700

    Doing work

--- client/src/main/java/example/client/ClientCommandLineRunner.java
+++ client/src/main/java/example/client/ClientCommandLineRunner.java
@@ -2,6 +2,8 @@ package example.client;
 
 import example.model.LetterRequest;
 import example.model.LetterResponse;
+import example.model.NumberRequest;
+import example.model.NumberResponse;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 import org.slf4j.Logger;
@@ -11,6 +13,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 
 /**
  * Runs the client application.
@@ -31,8 +35,16 @@ public class ClientCommandLineRunner implements CommandLineRunner {
                 .data(new LetterRequest(10))
                 .retrieveFlux(LetterResponse.class);
 
+        Flux<NumberResponse> randomNumbers = numberRSocketRequester.route("randomNumbers")
+                .data(new NumberRequest(10))
+                .retrieveFlux(NumberResponse.class);
+
         randomLetters.subscribe(letterResponse -> {
-            System.out.println(letterResponse.getLetter());
-        });
+            LOG.info("Letter Received: {}", letterResponse.getLetter());
+        }, Throwable::printStackTrace);
+
+        randomNumbers.subscribe(numberResponse -> {
+            LOG.info("Number Received: {}", numberResponse.getNumber());
+        }, Throwable::printStackTrace);
     }
 }
