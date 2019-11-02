e0a706f14ff3ac5bceca5edc1bb2e589f7201825
gregwhitaker <gwhitake@gmail.com>
2019-11-02 11:00:50 -0700

    Doing work

---
+++ client/src/main/java/example/client/config/ClientConfiguration.java
@@ -0,0 +1,45 @@
+package example.client.config;
+
+import io.rsocket.RSocket;
+import io.rsocket.RSocketFactory;
+import io.rsocket.frame.decoder.PayloadDecoder;
+import io.rsocket.transport.netty.client.TcpClientTransport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+import org.springframework.util.MimeTypeUtils;
+
+@Configuration
+public class ClientConfiguration {
+
+    @Bean
+    public RSocket letterRSocket() {
+        return RSocketFactory.connect()
+                .mimeType(MimeTypeUtils.APPLICATION_JSON_VALUE, MimeTypeUtils.APPLICATION_JSON_VALUE)
+                .frameDecoder(PayloadDecoder.ZERO_COPY)
+                .transport(TcpClientTransport.create(8081))
+                .start()
+                .block();
+    }
+
+    @Bean
+    public RSocket numberRSocket() {
+        return RSocketFactory.connect()
+                .mimeType(MimeTypeUtils.APPLICATION_JSON_VALUE, MimeTypeUtils.APPLICATION_JSON_VALUE)
+                .frameDecoder(PayloadDecoder.ZERO_COPY)
+                .transport(TcpClientTransport.create(8082))
+                .start()
+                .block();
+    }
+
+    @Bean
+    public RSocketRequester letterRSocketRequester(RSocketStrategies rSocketStrategies) {
+        return RSocketRequester.wrap(letterRSocket(), MimeTypeUtils.APPLICATION_JSON, MimeTypeUtils.APPLICATION_JSON, rSocketStrategies);
+    }
+
+    @Bean
+    public RSocketRequester numberRSocketRequester(RSocketStrategies rSocketStrategies) {
+        return RSocketRequester.wrap(numberRSocket(), MimeTypeUtils.APPLICATION_JSON, MimeTypeUtils.APPLICATION_JSON, rSocketStrategies);
+    }
+}
